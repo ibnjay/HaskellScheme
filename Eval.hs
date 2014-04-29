55aa352206f2a5a536f5bd1a852cdaa75655b343
Venkat Muruganandam <2011vmurugan@gmail.com>
2014-04-29 13:09:17 -0400

    Haskell Scheme Interpreter

---
+++ Eval.hs
@@ -0,0 +1,116 @@
+module Eval (eval, bindVars, apply, load) where 
+import Datatypes
+import Monad (liftM)
+import Data.IORef
+import Parser (parseSchemeList)
+
+eval :: Env -> LispVal -> IOThrowsError LispVal
+
+-- Primitives
+eval env val@(String _) = return val
+eval env val@(Number _) = return val
+eval env val@(Bool _) = return val
+eval env (List [Atom "quote", val]) = return val
+
+-- State-related Evaluators
+
+eval env (Atom id) = getVar env id
+eval env (List [Atom "set!", Atom var, form]) =
+    eval env form >>= setVar env var
+eval env (List [Atom "define", Atom var, form]) = 
+    eval env form >>= defineVar env var
+
+--If Clause
+
+eval env (List [Atom "if", pred, conseq, alt]) = 
+    do result <- eval env pred
+       case result of
+         Bool False -> eval env alt
+         otherwise -> eval env conseq
+-- Functions 
+
+eval env (List (Atom "define" : List (Atom var : params) : body)) =
+    makeNormalFunc env params body >>= defineVar env var
+eval env (List (Atom "define" : DottedList (Atom var : params) varargs : body)) =
+    makeVarargs varargs env params body >>= defineVar env var
+eval env (List (Atom "lambda" : List params : body)) =
+    makeNormalFunc env params body
+eval env (List (Atom "lambda" : DottedList params varargs : body)) =
+    makeVarargs varargs env params body
+eval env (List (Atom "lambda" : varargs@(Atom _) : body)) =
+    makeVarargs varargs env [] body
+
+-- IO
+eval env (List [Atom "load", String filename]) =
+    load filename >>= liftM last . mapM (eval env)
+
+-- Catch-all for user-defined functions
+eval env (List (function : args)) = do 
+    func <- eval env function
+    argVals <- mapM (eval env) args
+    apply func argVals
+
+-- Bad forms
+eval env badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+--State-related workhorses
+
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return. maybe False (const True) . lookup var
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do
+  env <- liftIO $ readIORef envRef
+  maybe (throwError $ UnboundVar "Getting an unbound variable: " var)
+        (liftIO . readIORef)
+        (lookup var env)
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do env <- liftIO $ readIORef envRef
+                             maybe (throwError $ UnboundVar "Setting an unbound variable: " var) 
+                                   (liftIO . (flip writeIORef value))
+                                   (lookup var env)
+                             return value
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do 
+    alreadyDefined <- liftIO $ isBound envRef var 
+    if alreadyDefined 
+       then setVar envRef var value >> return value
+       else liftIO $ do 
+          valueRef <- newIORef value
+          env <- readIORef envRef
+          writeIORef envRef ((var, valueRef) : env)
+          return value
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+    where extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+          addBinding (var, value) = do ref <- newIORef value
+                                       return (var, ref)
+
+
+
+-- Helpers for Functions
+apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
+apply (PrimitiveFunc func) args = liftThrows $ func args
+apply (Func params varargs body closure) args = 
+    if num params /= num args && varargs == Nothing
+       then throwError $ NumArgs (num params) args
+       else (liftIO $ bindVars closure $ zip params args) >>= bindVarArgs varargs >>= evalBody
+    where remainingArgs = drop (length params) args
+          num = toInteger . length
+          evalBody env = liftM last $ mapM (eval env) body
+          bindVarArgs arg env = case arg of 
+                                  Just argName -> liftIO $ bindVars env [(argName, List $ remainingArgs)]
+                                  Nothing -> return env
+apply (IOFunc func) args = func args
+
+
+makeFunc varargs env params body = return $ Func (map show params) varargs body env
+makeNormalFunc = makeFunc Nothing
+makeVarargs = makeFunc . Just . show
+
+-- IO Helpers
+load :: String -> IOThrowsError [LispVal]
+load filename = (liftIO $ readFile filename) >>= liftThrows . parseSchemeList
---
+++ Parser.hs
@@ -0,0 +1,175 @@
+module Parser ( parseScheme, parseSchemeList ) where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Numeric(readInt, readDec, readOct, readHex, readFloat)
+import Ratio
+import Complex
+import Char (digitToInt)
+import List (sort)
+import Monad (liftM)
+import qualified Data.Map as Map
+import Datatypes
+
+-- Supporting Functions
+
+readBin :: (Integral a) => ReadS a
+readBin = readInt 2 (\c -> c == '0' || c == '1') digitToInt
+
+fh :: [(a, b)] -> a
+fh x = fst (head (x))
+
+readBinFloat = readFloat -- Placeholders.
+readOctFloat = readFloat
+readHexFloat = readFloat
+
+--Supporting Definitions
+
+charIDs = Map.fromList([("space",' '),("newline",'\n'),("tab",'\t')])
+expChars = "sdflSDFL"
+
+--Shortcut Functions
+ 
+parseDec = parseNumber 10.0 (readDec, readFloat) digit
+parseBin = parseNumber 2.0 (readBin, readBinFloat) (oneOf ("01"))
+parseOct = parseNumber 8.0 (readOct, readOctFloat) octDigit
+parseHex = parseNumber 16.0 (readHex, readHexFloat) hexDigit
+
+
+-- Char
+symbol :: Parser Char
+symbol = initialSymbol <|> oneOf "+-.@"
+ 
+-- '+', '-', and '.' aren't allowed to be the first character of an atom
+initialSymbol :: Parser Char
+initialSymbol = oneOf "!$%&*/:<=>?^_~"
+ 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+escapeChar :: Parser Char
+escapeChar = do char '\\'
+                esc <- anyChar
+                return $ case esc of
+                           'n' -> '\n'
+                           'r' -> '\r'
+                           't' -> '\t'
+                           otherwise -> esc
+ 
+ 
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (escapeChar <|> noneOf "\"")
+                 char '"'
+                 return $ String x
+ 
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> initialSymbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = first:rest
+               return $ Atom atom
+
+-- These are special atoms defined in R5RS spec that begin with "unallowed" symbols
+parsePeculiarAtom :: Parser LispVal
+parsePeculiarAtom = liftM Atom $ try (string "...") <|> string "+" <|> string "-"
+
+parsePound :: Parser LispVal
+parsePound = do
+  char '#'
+  x <- anyChar
+  if (x == '\\') then
+      parseChar
+      else do
+          y <- (do {char '#'; anyChar} <|> return '#')
+          case (sort (x:y:[])) of
+            "#t" -> return $ Bool True
+            "#f" -> return $ Bool False
+            "#d" -> parseDec '?'
+            "#b" -> parseBin '?'
+            "#o" -> parseOct '?'
+            "#x" -> parseHex '?'
+            "#e" -> parseDec 'e'
+            "#i" -> parseDec 'i'
+            "di" -> parseDec 'i'
+            "bi" -> parseBin 'i'
+            "io" -> parseOct 'i'
+            "ix" -> parseHex 'i'
+            "de" -> parseDec 'e'
+            "be" -> parseBin 'e'
+            "eo" -> parseOct 'e'
+            "ex" -> parseHex 'e'
+
+parseChar :: Parser LispVal
+parseChar = do ident <- many (noneOf " ")
+               case length ident of
+                 0 -> return $ Char ' ' -- Will eventually need to be more robust.
+                 1 -> return $ Char (head ident)
+                 _ -> return $ Char ((\(Just x) -> x) (Map.lookup ident charIDs))
+
+--I'll let Haskell infer for now
+parseNumber b (f,g) d p' = do 
+  predec <- many1 d
+  dec <- (do {char '.'; many d} <|> return "!")
+  expchar <- (do {oneOf expChars} <|> return '!')
+  exp <- (if expchar /= '!' then many d else return "0")
+  let p = (if p' == '?' then if (dec /= "!" || expchar /= '!') then 'i' else 'e' else p')
+  let r = if (dec == "!" && expchar == '!') then (Number ((fh (f ("0" ++ predec))) * (round b) ^ (fh (f exp))))
+              else if p == 'i' then (Float (((fh (g ("0" ++ predec ++ "." ++ dec ++ "0" )))) * b ^ (fh (f exp))))
+                       else (Rational ((((fh (f ("0" ++ predec))) % 1) + ((fh (f ("0" ++ dec))) % (round b) ^ (length dec)) * (((round b) ^ (fh (f (exp)))) % 1))))
+  return r
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parsePeculiarAtom
+        <|> parseString
+        <|> parseDec '?'
+        <|> parsePound
+        <|> parseQuoted
+        <|> parseQuasiQuoted
+        <|> parseUnquoteSplicing
+        <|> parseUnquote
+        <|> parseAnyList
+
+parseAnyList :: Parser LispVal
+parseAnyList = do p <- char '('
+                  many space
+                  x <- sepEndBy parseExpr spaces
+                  c <- char '.' <|> char ')'
+                  if c == '.' then do 
+                                y <- spaces >> parseExpr
+                                many space
+                                char ')'
+                                return $ DottedList x y
+                    else do return $ List x
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+parseQuasiQuoted :: Parser LispVal
+parseQuasiQuoted = do
+  char '`'
+  x <- parseExpr
+  return $ List [Atom "quasiquote", x]
+
+parseUnquote :: Parser LispVal
+parseUnquote = do
+  try (char ',')
+  x <- parseExpr
+  return $ List [Atom "unquote", x]
+
+parseUnquoteSplicing :: Parser LispVal
+parseUnquoteSplicing = do
+  try (string ",@")
+  x <- parseExpr
+  return $ List [Atom "unquote-splicing", x]
+
+-- Parsing logic
+parseGeneric :: Parser a -> String -> ThrowsError a
+parseGeneric parser input = case parse parser "scheme" input of
+                              Left err -> throwError $ Parser err
+                              Right val -> return val
+
+
+parseScheme = parseGeneric parseExpr
+parseSchemeList = parseGeneric $ do sepEndBy (try parseExpr) spaces
