af89a44d0ac49a9be2087ff0ef6ddf3d93b776d5
Michael Gurlitz <michael@gurlitz.org>
2014-05-04 00:06:16 -0400

    Improvements to hangman example

--- Examples/hangman.scm
+++ Examples/hangman.scm
@@ -1,38 +1,50 @@
 (load "stdlib.scm")
 
-; track guessed letters
-; [String] -> String -> String (with underscores)
-
 (define (show-status letters word)
     (string-concat
         (map
-          (lambda (x) (if (in-array x letters) x "_ "))
+          (lambda (x) (if (in-array x letters)
+            (string-cons x "") "_ "))
         (char-list word))))
 
 (define (get-word pred)
-    (define contents (apply read-contents "three.txt"))
+    (define contents (apply read-contents "input/three.txt"))
     (define lines (string-split "\n" contents))
     (define words (concat (map (lambda (x) (string-split " " x)) lines)))
     (define words2 (filter pred words))
     (define idx (get-random 0 (length words2)))
     (list-index idx words2))
 
+(define (game picked-letters word)
+    (write-line "")
+    (write-line (show-status picked-letters word))
+    
+    (write "Guess a letter: ")
+    (define guess (apply read-line))
+    (write (string-append "You guessed: " guess))
+    (define letter (string-charat 0 guess))
+
+    (write-line (if (in-array letter (char-list word))
+        "... success!" "... try again!"))
+    (print letter)
+    (print (in-array letter (char-list word)))
+    (print (char-list word))
+    (game (cons letter picked-letters) word)
+    )
+
 (define (main)
     (write "Pick a difficulty [easy/medium/hard]: ")
-    ; (define input (apply read-line))
-    (define input "hard")
+    (define input (apply read-line))
 
     (define pred
         (if (string=? input "easy")
             (lambda (word) (between? 1 6 (string-length word)))
-            (lambda (word) (between? 6 10 (string-length word)))
-              ))
+            (if (string=? input "medium")
+                (lambda (word) (between? 6 8 (string-length word)))
+                (lambda (word) (between? 8 9999 (string-length word)))
+            )))
     (define theword (get-word pred))
-
-    (write-line (show-status '() theword))
-    (write "Guess a letter: ")
-    (define result (apply read-line))
-
-    (print theword))
+    (write-line (string-append "The word is: " theword))
+    (game '() theword))
 
 (main)
